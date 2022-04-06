5bdb6a7a85ba0e97d98ec37a5a8c0940718a1c57
Mert Özdemir <mertozdmr91@gmail.com>
2022-04-06 17:54:47 +0300

    firstcommit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Popular from '../component/Popular'
+import Veggie from '../component/Veggie'
+
+function Home() {
+  return (
+    <div>
+        <Popular />
+        <Veggie />
+    </div>
+  )
+}
+
+export default Home
