5572815fc74e39ee7a72824877371e96e5d114b0
Mert Özdemir <mertozdmr91@gmail.com>
2022-04-07 18:01:53 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Category from "./component/Category";
 import Pages from "./pages/Pages";
-
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Category />
         <Pages />
+      </BrowserRouter>
     </div>
   );
 }
